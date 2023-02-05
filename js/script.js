cda9e9fdacff655c90ee6cc827045aaa5408f803
Junior Dantas <116525934+juniord-dev@users.noreply.github.com>
2023-02-05 15:16:13 -0300

    Integrando JavaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log('Teste no console do navegador.');
+function start() {
+  var buttonCalculateImc = document.querySelector('#button-calculate-imc');
+  buttonCalculateImc.addEventListener('click', handleButtonClick);
+
+  var inputWeight = document.querySelector('#input-weight');
+  var inputHeight = document.querySelector('#input-height');
+  
+  inputWeight.addEventListener('input', handleButtonClick);
+  inputHeight.addEventListener('input', handleButtonClick);
+
+  handleButtonClick();
+}
+
+function calculateImc(weight, height) {
+  return (weight / (height * height)).toFixed(2).replace('.', ',');
+}
+function handleButtonClick() {
+  var inputWeight = document.querySelector('#input-weight');
+  var inputHeight = document.querySelector('#input-height');
+
+  var imcResult = document.querySelector('#imcResult');
+
+  var weight = Number(inputWeight.value);
+  var height = Number(inputHeight.value);
+
+  var imc = calculateImc(weight, height);
+  imcResult.textContent = imc;
+}
+
+start();
